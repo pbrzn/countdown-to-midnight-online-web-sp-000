497418d099d20b364f0122abfb8e144e6b23be5f
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-15 03:32:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
-
-def countdown
+def countdown(num)
+  while num>0
+    num-=1
+    "#{num+1} SECOND(S)!"
+  end
+  if num==0
+    "HAPPY NEW YEAR!"
+  end
 end
+
+def countdown_with_sleep(int)
+  x=10
+  while x>0
+    x-=1
+    sleep(int)
+    "#{x} SECOND(S)!"
+  end
+  if x==1
+    "HAPPY NEW YEAR!"
+  end
+end
+
+countdown_with_sleep(5)
